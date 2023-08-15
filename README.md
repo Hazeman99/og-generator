19291c7c066c2a2f731012213daf79f6b67ee337
Hazem Hadi <hhadi@safedecision.com.sa>
2023-08-15 11:11:39 +0300

    completed main functionality of the app

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
 # OG Portfolio Image Generator CLI Tool
 
-Heavily relies on the article:
+## Description
+
+Create an OG image for an article on your website with this CLI tool.
+
+**Note: The code is pretty messy and needs to be refactored.**
+
+## Pre-requisites
+
+- Go 1.18
+- Background Image - 1200 X 630 pixels
+- Icon Image - 48 X 48 pixels
+
+## Usage
+
+Make sure that you have the **Background Image** (file named background.png) and **Icon Image** (file names icon.png) in the same directory as the executable.
+
+Run the following command:
+
+```bash
+go run . --title="<TITLE HERE>" --description="<DESCRIPTION HERE>" --minutes=7 --author="<AUTHOR NAME HERE>" --website="<WEBSITE URL HERE>"
+```
+
+Or you can build the executable and run it:
+
+```bash
+go build .
+./og-image-generator --title="<TITLE HERE>" --description="<DESCRIPTION HERE>" --minutes=7 --author="<AUTHOR NAME HERE>" --website="<WEBSITE URL HERE>"
+```
+
+## Credits
+
+Heavily relies on the below referenced article:
 https://pace.dev/blog/2020/03/02/dynamically-generate-social-images-in-golang-by-mat-ryer.html
--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"errors"
+	"flag"
+	"fmt"
 	"image/color"
 	"os"
 	"path/filepath"
@@ -10,6 +12,7 @@ import (
 )
 
 func main() {
+	// Setup the canvas
 	width := 1200
 	height := 630
 
@@ -21,27 +24,63 @@ func main() {
 
 	addOverlay(dc)
 
-	fontPath := filepath.Join("fonts", "OpenSans-Regular.ttf")
+	fontPath := filepath.Join("fonts", "OpenSans-Bold.ttf")
 	if err := dc.LoadFontFace(fontPath, 50); err != nil {
 		panic(errors.New("unable to load font"))
 	}
 	dc.SetColor(color.Black)
 
-	s := os.Args[1]
-	addTitle(dc, s)
+	// Parse args from the command line
+	title := flag.String("title", "", "title of the article")
+	description := flag.String("description", "", "description of the article")
+	author := flag.String("author", "", "author of the article")
+	minutes := flag.Int("minutes", 0, "minutes to read the article")
+	website := flag.String("website", "", "website of the article")
 
-	if err := dc.LoadFontFace(fontPath, 45); err != nil {
+	flag.Parse()
+
+	addTitle(dc, *title)
+
+	fontPath = filepath.Join("fonts", "OpenSans-Regular.ttf")
+	if err := dc.LoadFontFace(fontPath, 34); err != nil {
 		panic(errors.New("unable to load font"))
 	}
 
-	website := "https://hazemhadi.com"
-	addWebsite(dc, website)
+	// set the color to gray
+	dc.SetHexColor("#4a4a4a")
+	addDescription(dc, *description, len(*title))
 
-	dc.SavePNG("./output.png")
+	if err := dc.LoadFontFace(fontPath, 38); err != nil {
+		panic(errors.New("unable to load font"))
+	}
+
+	addMinutesAndAuthor(dc, *minutes, *author)
+
+	addWebsite(dc, *website)
+
+	// if icon does not exist, it will not be added
+	_, err := os.Stat("./icon.png")
+	if err != nil {
+		fmt.Println("Error getting file. Skipping...")
+	}
+
+	if err == nil {
+		err = addIcon(dc, "./icon.png")
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	err = dc.SavePNG("./output.png")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully generated image. Check output.png")
 }
 
 func loadBackgroundImage(dc *gg.Context) (err error) {
-	backgroundImage, err := gg.LoadImage("./OG-Portfolio-BaseImage.png")
+	backgroundImage, err := gg.LoadImage("./background.png")
 	if err != nil {
 		return err
 	}
@@ -64,11 +103,51 @@ func addOverlay(dc *gg.Context) {
 
 func addTitle(dc *gg.Context, title string) {
 	textRightMargin := 60.0
-	textTopMargin := 90.0
+	textTopMargin := 100.0
 	x := textRightMargin
 	y := textTopMargin
 	maxWidth := float64(dc.Width()) - textRightMargin - textRightMargin
 	dc.DrawStringWrapped(title, x, y, 0, 0, maxWidth, 1.5, gg.AlignLeft)
+}
+
+func addDescription(dc *gg.Context, description string, titleLength int) {
+	// take into account that it depends on Title for where it starts
+	textRightMargin := 60.0
+	textTopMargin := 100.0
+	x := textRightMargin
+	// split string into letters and count the number of lines
+	if titleLength > 50 {
+		textTopMargin = 140.0
+	}
+
+	y := textTopMargin + 80
+	maxWidth := float64(dc.Width()) - textRightMargin - textRightMargin
+
+	if len(description) > 197 {
+		description = description[:197] + "..."
+	}
+
+	dc.DrawStringWrapped(description, x, y, 0, 0, maxWidth, 1.8, gg.AlignLeft)
+}
+
+func addMinutesAndAuthor(dc *gg.Context, minutes int, author string) {
+	textColor := color.Black
+	r, g, b, _ := textColor.RGBA()
+	mutedColor := color.RGBA{
+		R: uint8(r),
+		G: uint8(g),
+		B: uint8(b),
+		A: uint8(200),
+	}
+	dc.SetColor(mutedColor)
+
+	text := fmt.Sprintf("%d min read • %s", minutes, author)
+
+	_, textHeight := dc.MeasureString(text)
+	marginY := 110.0
+	x := 70.0
+	y := float64(dc.Height()) - textHeight - marginY
+	dc.DrawString(text, x, y)
 }
 
 func addWebsite(dc *gg.Context, website string) {
@@ -87,4 +166,15 @@ func addWebsite(dc *gg.Context, website string) {
 	x := 70.0
 	y := float64(dc.Height()) - textHeight - marginY
 	dc.DrawString(website, x, y)
+}
+
+func addIcon(dc *gg.Context, iconPath string) (err error) {
+	icon, err := gg.LoadImage(iconPath)
+	if err != nil {
+		return err
+	}
+
+	dc.DrawImage(icon, dc.Width()-100, dc.Height()-100)
+
+	return nil
 }
